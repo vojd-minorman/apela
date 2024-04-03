7e9622cbe26767df402682f53679587bcca1027f
Neville Mitshiabu <agencewonorogo@gmail.com>
2024-04-03 08:45:53 +0100

    Ajout des dernières modifications (troisième section)

---
+++ assets/js/owl-me.js
@@ -0,0 +1,19 @@
+$('#actus-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:false,
+    margin:1,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+  })
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,4 @@
+//Function pour le bouton du menu
 function myFunction(x) {
   x.classList.toggle("change");}
+
