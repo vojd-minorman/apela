49c5b37401e5e84ffdadcdbebd6539396fc676f7
Neville Mitshiabu <agencewonorogo@gmail.com>
2024-04-14 15:17:10 +0100

    Ajout des nouvelles sections

--- assets/js/owl-me.js
+++ assets/js/owl-me.js
@@ -1,19 +1,41 @@
 $('#actus-carousel').owlCarousel({
     stagePadding: 50,
-    loop:false,
-    margin:1,
+    loop: false,
+    margin:20,
     nav:false,
     dots: false,
     responsive:{
         0:{
-            items:2
+            items:1,
+            loop : true,
+            margin: 10
         },
         600:{
-            items:4
+            items:3
         },
         1000:{
-            items:5
+            items:4
         }
     }
   })
 
+  $('#activites-carousel').owlCarousel({
+    stagePadding: 50,
+    loop: false,
+    margin:20,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1,
+            loop : true,
+            margin: 10
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+  })
